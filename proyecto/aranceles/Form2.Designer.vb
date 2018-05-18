b8139f4d69f51d2034e0053fc1dfde7253c3228a
RenzoMinelli <39157163+RenzoMinelli@users.noreply.github.com>
2018-05-18 18:17:07 -0300

    soluciones xd

--- proyecto/aranceles/Form2.Designer.vb
+++ proyecto/aranceles/Form2.Designer.vb
@@ -22,17 +22,8 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.SuspendLayout()
-        '
-        'Form4
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        components = New System.ComponentModel.Container
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-<<<<<<< HEAD:proyecto/aranceles/Form2.Designer.vb
         Me.Text = "Form2"
-=======
-       
-
-
     End Sub
 End Class
