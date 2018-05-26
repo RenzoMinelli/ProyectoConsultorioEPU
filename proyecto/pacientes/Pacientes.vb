e8809a6abfb6a0c4d3a103e71b98f7ab92c9ae7e
MateoIzaguirre22 <mateoizaguirre22@outlook.com>
2018-05-26 15:50:37 -0300

    agregando cositas :alien:

--- proyecto/pacientes/Pacientes.vb
+++ proyecto/pacientes/Pacientes.vb
@@ -9,6 +9,7 @@
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Me.Size = New System.Drawing.Size(850, 500)
         actTabla()
+        Label20.ForeColor = Color.Gold
     End Sub
 
     Private Sub DataGridView1_CellClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellClick
