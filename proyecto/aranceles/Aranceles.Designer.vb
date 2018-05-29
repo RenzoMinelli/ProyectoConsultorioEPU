92c7e67a2e5b3c9db3d06b72f881b6373d08971e
RenzoMinelli <39157163+RenzoMinelli@users.noreply.github.com>
2018-05-28 22:44:50 -0300

    cambio en tabla
    le puse para que no se pueda modificar, haz esto en todas tus tablas mateo xddd

--- proyecto/aranceles/Aranceles.Designer.vb
+++ proyecto/aranceles/Aranceles.Designer.vb
@@ -67,6 +67,7 @@ Partial Class Aranceles
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Location = New System.Drawing.Point(43, 28)
         Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
         Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.DataGridView1.Size = New System.Drawing.Size(340, 256)
         Me.DataGridView1.TabIndex = 0
