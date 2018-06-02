01ff954056b281f00b8e90418e1b7b8a5b69a5f3
RenzoMinelli <39157163+RenzoMinelli@users.noreply.github.com>
2018-06-02 20:19:25 -0300

    xd

--- proyecto/pacientes/Pacientes.Designer.vb
+++ proyecto/pacientes/Pacientes.Designer.vb
@@ -94,6 +94,7 @@ Partial Class Pacientes
         Me.DataGridView1.Location = New System.Drawing.Point(169, 90)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersVisible = False
         Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.DataGridView1.Size = New System.Drawing.Size(188, 274)
         Me.DataGridView1.TabIndex = 1
