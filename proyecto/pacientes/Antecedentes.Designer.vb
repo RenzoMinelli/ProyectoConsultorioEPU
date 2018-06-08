c21f9c6f60de343811a8269a06330536735fe76d
Ana Balatti <balatti.2014@gmail.com>
2018-06-08 17:26:37 -0300

    COSAS

--- proyecto/pacientes/Antecedentes.Designer.vb
+++ proyecto/pacientes/Antecedentes.Designer.vb
@@ -32,14 +32,16 @@ Partial Class Antecedentes
         Me.CheckBox2 = New System.Windows.Forms.CheckBox()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.tablita = New System.Windows.Forms.DataGridView()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         CType(Me.tablita, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'CheckBox9
         '
         Me.CheckBox9.AutoSize = True
         Me.CheckBox9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox9.Location = New System.Drawing.Point(196, 339)
+        Me.CheckBox9.Location = New System.Drawing.Point(40, 341)
         Me.CheckBox9.Name = "CheckBox9"
         Me.CheckBox9.Size = New System.Drawing.Size(168, 24)
         Me.CheckBox9.TabIndex = 20
@@ -50,7 +52,7 @@ Partial Class Antecedentes
         '
         Me.CheckBox8.AutoSize = True
         Me.CheckBox8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox8.Location = New System.Drawing.Point(196, 299)
+        Me.CheckBox8.Location = New System.Drawing.Point(40, 301)
         Me.CheckBox8.Name = "CheckBox8"
         Me.CheckBox8.Size = New System.Drawing.Size(130, 24)
         Me.CheckBox8.TabIndex = 19
@@ -61,7 +63,7 @@ Partial Class Antecedentes
         '
         Me.CheckBox7.AutoSize = True
         Me.CheckBox7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox7.Location = New System.Drawing.Point(196, 263)
+        Me.CheckBox7.Location = New System.Drawing.Point(40, 265)
         Me.CheckBox7.Name = "CheckBox7"
         Me.CheckBox7.Size = New System.Drawing.Size(149, 24)
         Me.CheckBox7.TabIndex = 18
@@ -72,7 +74,7 @@ Partial Class Antecedentes
         '
         Me.CheckBox6.AutoSize = True
         Me.CheckBox6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox6.Location = New System.Drawing.Point(196, 230)
+        Me.CheckBox6.Location = New System.Drawing.Point(40, 232)
         Me.CheckBox6.Name = "CheckBox6"
         Me.CheckBox6.Size = New System.Drawing.Size(100, 24)
         Me.CheckBox6.TabIndex = 17
@@ -83,7 +85,7 @@ Partial Class Antecedentes
         '
         Me.CheckBox5.AutoSize = True
         Me.CheckBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox5.Location = New System.Drawing.Point(196, 196)
+        Me.CheckBox5.Location = New System.Drawing.Point(40, 198)
         Me.CheckBox5.Name = "CheckBox5"
         Me.CheckBox5.Size = New System.Drawing.Size(116, 24)
         Me.CheckBox5.TabIndex = 16
@@ -94,7 +96,7 @@ Partial Class Antecedentes
         '
         Me.CheckBox4.AutoSize = True
         Me.CheckBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox4.Location = New System.Drawing.Point(196, 160)
+        Me.CheckBox4.Location = New System.Drawing.Point(40, 162)
         Me.CheckBox4.Name = "CheckBox4"
         Me.CheckBox4.Size = New System.Drawing.Size(103, 24)
         Me.CheckBox4.TabIndex = 15
@@ -105,7 +107,7 @@ Partial Class Antecedentes
         '
         Me.CheckBox3.AutoSize = True
         Me.CheckBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox3.Location = New System.Drawing.Point(196, 123)
+        Me.CheckBox3.Location = New System.Drawing.Point(40, 125)
         Me.CheckBox3.Name = "CheckBox3"
         Me.CheckBox3.Size = New System.Drawing.Size(173, 24)
         Me.CheckBox3.TabIndex = 14
@@ -116,7 +118,7 @@ Partial Class Antecedentes
         '
         Me.CheckBox2.AutoSize = True
         Me.CheckBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox2.Location = New System.Drawing.Point(196, 85)
+        Me.CheckBox2.Location = New System.Drawing.Point(40, 87)
         Me.CheckBox2.Name = "CheckBox2"
         Me.CheckBox2.Size = New System.Drawing.Size(154, 24)
         Me.CheckBox2.TabIndex = 13
@@ -128,7 +130,7 @@ Partial Class Antecedentes
         Me.CheckBox1.AutoSize = True
         Me.CheckBox1.Enabled = False
         Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox1.Location = New System.Drawing.Point(196, 45)
+        Me.CheckBox1.Location = New System.Drawing.Point(40, 48)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(142, 33)
         Me.CheckBox1.TabIndex = 12
@@ -146,15 +148,24 @@ Partial Class Antecedentes
         Me.tablita.Enabled = False
         Me.tablita.Location = New System.Drawing.Point(12, 12)
         Me.tablita.Name = "tablita"
-        Me.tablita.Size = New System.Drawing.Size(26, 17)
+        Me.tablita.Size = New System.Drawing.Size(31, 21)
         Me.tablita.TabIndex = 21
         Me.tablita.Visible = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(261, 33)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 338)
+        Me.DataGridView1.TabIndex = 22
         '
         'Antecedentes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(564, 408)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.tablita)
         Me.Controls.Add(Me.CheckBox9)
         Me.Controls.Add(Me.CheckBox8)
@@ -168,6 +179,7 @@ Partial Class Antecedentes
         Me.Name = "Antecedentes"
         Me.Text = "Antecedentes2"
         CType(Me.tablita, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -182,4 +194,5 @@ Partial Class Antecedentes
     Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
     Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
     Friend WithEvents tablita As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
 End Class
--- proyecto/pacientes/Antecedentes.vb
+++ proyecto/pacientes/Antecedentes.vb
@@ -4,6 +4,42 @@
         Consulta = "select * from antecedentes where id_p = '" + Str(id_p) + "';"
         consultar()
         tablita.DataSource = Tabla
+        Dim alergicos As Boolean = tablita.Rows(0).Cells(1).Value
+        Dim fiebre_reumatica As Boolean = tablita.Rows(0).Cells(2).Value
+        Dim farmacos_recibidos As Boolean = tablita.Rows(0).Cells(3).Value
+        Dim diabeticos As Boolean = tablita.Rows(0).Cells(4).Value
+        Dim coagulacion As Boolean = tablita.Rows(0).Cells(5).Value
+        Dim familiares As Boolean = tablita.Rows(0).Cells(6).Value
+        Dim cardiovasculares As Boolean = tablita.Rows(0).Cells(7).Value
+        Dim odontologicos As Boolean = tablita.Rows(0).Cells(8).Value
+        Dim tratamiento_medico As Boolean = tablita.Rows(0).Cells(9).Value
+
+        DataGridView1.ColumnCount = 2
+        DataGridView1.RowCount = 9
+
+        DataGridView1.Rows(0).Cells(0).Value = "alergicos"
+        If alergicos = "true" Then
+            DataGridView1.Rows(0).Cells(1).Value = GetType(Boolean)
+        Else
+            DataGridView1.Rows(0).Cells(1).Value = False
+        End If
+        DataGridView1.Rows(1).Cells(0).Value = "fiebre reumatica"
+        DataGridView1.Rows(1).Cells(1).Value = fiebre_reumatica
+        DataGridView1.Rows(2).Cells(0).Value = "farmacos recibidos"
+        DataGridView1.Rows(2).Cells(1).Value = farmacos_recibidos
+        DataGridView1.Rows(3).Cells(0).Value = "diabeticos"
+        DataGridView1.Rows(3).Cells(1).Value = diabeticos
+        DataGridView1.Rows(4).Cells(0).Value = "coagulacion"
+        DataGridView1.Rows(4).Cells(1).Value = coagulacion
+        DataGridView1.Rows(5).Cells(0).Value = "familiares"
+        DataGridView1.Rows(5).Cells(1).Value = familiares
+        DataGridView1.Rows(6).Cells(0).Value = "cardiovasculares"
+        DataGridView1.Rows(6).Cells(1).Value = cardiovasculares
+        DataGridView1.Rows(7).Cells(0).Value = "odontologicos"
+        DataGridView1.Rows(7).Cells(1).Value = odontologicos
+        DataGridView1.Rows(8).Cells(0).Value = "tratamiento medico"
+        DataGridView1.Rows(8).Cells(1).Value = tratamiento_medico
+
 
         If tablita.Rows(0).Cells(1).Value = True Then
             CheckBox1.Checked = True
