f24f656506e64fff7661d7712576e74ab5fe57b1
Nicomauxi <38508835+Nicomauxi@users.noreply.github.com>
2018-06-27 17:02:45 -0300

    pipi

--- proyecto/citas/Citas.vb
+++ proyecto/citas/Citas.vb
@@ -5,6 +5,7 @@
         a = My.Resources.rambocursor
         Me.Cursor = New Cursor(a.Handle)
 
+        MsgBox(Str(AxCalendar1.Day))
     End Sub
 
     Private Sub Label19_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
@@ -15,5 +16,13 @@
     Private Sub Label20_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Me.Dispose()
         Pacientes.Show()
+    End Sub
+
+    Private Sub AxCalendar1_OnDayClick(ByVal sender As System.Object, ByVal e As AxCALENDARLib._DCalendarEvents_OnDayClickEvent) Handles AxCalendar1.OnDayClick
+        MsgBox(Str(AxCalendar1.Day))
+    End Sub
+
+    Private Sub SaveFileDialog3_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs)
+
     End Sub
 End Class
