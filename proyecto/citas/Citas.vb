dc5a4444bc709c3d7f9a4d6aca3e0b70bcf8fad4
Renzo Minelli <renminelli@gmail.com>
2018-06-27 17:31:28 -0300

    vdfvsd

--- proyecto/citas/Citas.vb
+++ proyecto/citas/Citas.vb
@@ -1,19 +1,13 @@
 ﻿Public Class Citas
     Dim cita As String
     Private Sub Citas_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-<<<<<<< HEAD
 
-=======
->>>>>>> f24f656506e64fff7661d7712576e74ab5fe57b1
         Dim a As Icon
         a = My.Resources.rambocursor
         Me.Cursor = New Cursor(a.Handle)
 
-<<<<<<< HEAD
-
-=======
         MsgBox(Str(AxCalendar1.Day))
->>>>>>> f24f656506e64fff7661d7712576e74ab5fe57b1
+
     End Sub
 
     Private Sub Label19_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
