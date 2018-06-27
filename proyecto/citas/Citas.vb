1a3d89999c1c031a12ce2bd41e0f53be189b7b62
Renzo Minelli <renminelli@gmail.com>
2018-06-27 17:29:14 -0300

    hola

--- proyecto/citas/Citas.vb
+++ proyecto/citas/Citas.vb
@@ -1,12 +1,12 @@
 ﻿Public Class Citas
     Dim cita As String
     Private Sub Citas_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-<<<<<<< HEAD
+
         Dim a As Icon
         a = My.Resources.rambocursor
         Me.Cursor = New Cursor(a.Handle)
-=======
->>>>>>> 306a2cf62dc24dc95f016a6a25636e3910ecfddf
+
+
     End Sub
 
     Private Sub Label19_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
