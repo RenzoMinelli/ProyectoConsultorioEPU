e5b6d92856f1ba98fe58341b8a1da11a7c0801b8
Renzo Minelli <renminelli@gmail.com>
2018-07-22 22:22:20 -0300

    agregue la funcion para comprobar que es una cedula valida

--- proyecto/pacientes/Agregar_Paciente.vb
+++ proyecto/pacientes/Agregar_Paciente.vb
@@ -24,16 +24,55 @@
         Dim ver2 As Integer = 0
         If TextBox1.Text <> "" And TextBox2.Text <> "" And TextBox4.Text <> "" And telefonot.Text <> "" Then
             If IsNumeric(TextBox2.Text) Then
-                If alergicos = False And diabeticos = False And cardiovasculares = False And fiebre_reumatica = False And coagulacion = False And odontologicos = False And farmacos_recibidos = False And familiares = False And tratamiento_medico = False And obs = "" Then
-                    If MsgBox("No fue ingresado ningún antecedente," + vbNewLine + "¿Desea continuar de todos modos?", MsgBoxStyle.YesNo) = vbYes Then
+                If verCed(TextBox2.Text) = True Then
+                    If alergicos = False And diabeticos = False And cardiovasculares = False And fiebre_reumatica = False And coagulacion = False And odontologicos = False And farmacos_recibidos = False And familiares = False And tratamiento_medico = False And obs = "" Then
+                        If MsgBox("No fue ingresado ningún antecedente," + vbNewLine + "¿Desea continuar de todos modos?", MsgBoxStyle.YesNo) = vbYes Then
 
+                            nombre = TextBox1.Text
+                            cedula = TextBox2.Text
+                            If TextBox3.Text <> "" Then
+                                ver = 1
+                                enviado = TextBox3.Text
+                            End If
+                            telefono = telefonot.Text
+                            direccion = TextBox4.Text
+
+                            If TextBox5.Text <> "" Then
+                                direTra = TextBox5.Text
+                                ver2 = 1
+                            End If
+
+                            nac = MonthCalendar1.SelectionRange.Start.ToString("yyyy-MM-dd")
+
+
+                            If ver = 0 And ver2 = 0 Then
+                                Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, telefono, direccion_particular) values('" + cedula + "','" + nac + "','" + nombre + "', '" + telefono + "','" + direccion + "');"
+                            ElseIf ver = 0 And ver2 = 1 Then
+                                Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, telefon, direccion_particular, direccion_trabajo) values('" + cedula + "','" + nac + "','" + nombre + "', '" + telefono + "','" + direccion + "','" + direTra + "', '" + telefono + "');"
+                            ElseIf ver = 1 And ver2 = 0 Then
+                                Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, telefono, enviado_por, direccion_particular) values('" + cedula + "','" + nac + "','" + nombre + "', '" + telefono + "','" + enviado + "','" + direccion + "', '" + telefono + "');"
+                            ElseIf ver = 1 And ver2 = 1 Then
+                                Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, telefono, enviado_por, direccion_particular, direccion_trabajo) values('" + cedula + "','" + nac + "','" + nombre + "', '" + telefono + "','" + enviado + "','" + direccion + "','" + direTra + "', '" + telefono + "');"
+                            End If
+
+
+                            consultar()
+
+                            MsgBox("Agregado con Exito")
+
+                            Me.Dispose()
+                            Pacientes.Show()
+                            Pacientes.actTabla()
+
+                        End If
+                    Else
                         nombre = TextBox1.Text
                         cedula = TextBox2.Text
                         If TextBox3.Text <> "" Then
                             ver = 1
                             enviado = TextBox3.Text
                         End If
-                        telefono = telefonot.Text
+
                         direccion = TextBox4.Text
 
                         If TextBox5.Text <> "" Then
@@ -45,73 +84,39 @@
 
 
                         If ver = 0 And ver2 = 0 Then
-                            Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, telefono, direccion_particular) values('" + cedula + "','" + nac + "','" + nombre + "', '" + telefono + "','" + direccion + "');"
+                            Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, direccion_particular) values('" + cedula + "','" + nac + "','" + nombre + "','" + direccion + "');"
                         ElseIf ver = 0 And ver2 = 1 Then
-                            Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, telefon, direccion_particular, direccion_trabajo) values('" + cedula + "','" + nac + "','" + nombre + "', '" + telefono + "','" + direccion + "','" + direTra + "', '" + telefono + "');"
+                            Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, direccion_particular, direccion_trabajo) values('" + cedula + "','" + nac + "','" + nombre + "','" + direccion + "','" + direTra + "');"
                         ElseIf ver = 1 And ver2 = 0 Then
-                            Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, telefono, enviado_por, direccion_particular) values('" + cedula + "','" + nac + "','" + nombre + "', '" + telefono + "','" + enviado + "','" + direccion + "', '" + telefono + "');"
+                            Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, enviado_por, direccion_particular) values('" + cedula + "','" + nac + "','" + nombre + "','" + enviado + "','" + direccion + "');"
                         ElseIf ver = 1 And ver2 = 1 Then
-                            Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, telefono, enviado_por, direccion_particular, direccion_trabajo) values('" + cedula + "','" + nac + "','" + nombre + "', '" + telefono + "','" + enviado + "','" + direccion + "','" + direTra + "', '" + telefono + "');"
+                            Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, enviado_por, direccion_particular, direccion_trabajo) values('" + cedula + "','" + nac + "','" + nombre + "','" + enviado + "','" + direccion + "','" + direTra + "');"
                         End If
 
 
                         consultar()
+
+                        Consulta = "select id_p from paciente;"
+                        consultar()
+
+                        DataGridView1.DataSource = Tabla
+                        id_p = DataGridView1.Rows(DataGridView1.RowCount - 1).Cells(0).Value
+
+
+                        Consulta = "INSERT INTO antecedentes VALUES ('" + Str(id_p) + "','" + alergicos.GetHashCode.ToString + "','" + diabeticos.GetHashCode.ToString + "','" + cardiovasculares.GetHashCode.ToString + "','" + fiebre_reumatica.GetHashCode.ToString + "','" + coagulacion.GetHashCode.ToString + "','" + odontologicos.GetHashCode.ToString + "','" + farmacos_recibidos.GetHashCode.ToString + "','" + familiares.GetHashCode.ToString + "','" + tratamiento_medico.GetHashCode.ToString + "','" + obs + "');"
+                        consultar()
+
 
                         MsgBox("Agregado con Exito")
 
                         Me.Dispose()
                         Pacientes.Show()
                         Pacientes.actTabla()
-
                     End If
                 Else
-                    nombre = TextBox1.Text
-                    cedula = TextBox2.Text
-                    If TextBox3.Text <> "" Then
-                        ver = 1
-                        enviado = TextBox3.Text
-                    End If
-
-                    direccion = TextBox4.Text
-
-                    If TextBox5.Text <> "" Then
-                        direTra = TextBox5.Text
-                        ver2 = 1
-                    End If
-
-                    nac = MonthCalendar1.SelectionRange.Start.ToString("yyyy-MM-dd")
-
-
-                    If ver = 0 And ver2 = 0 Then
-                        Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, direccion_particular) values('" + cedula + "','" + nac + "','" + nombre + "','" + direccion + "');"
-                    ElseIf ver = 0 And ver2 = 1 Then
-                        Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, direccion_particular, direccion_trabajo) values('" + cedula + "','" + nac + "','" + nombre + "','" + direccion + "','" + direTra + "');"
-                    ElseIf ver = 1 And ver2 = 0 Then
-                        Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, enviado_por, direccion_particular) values('" + cedula + "','" + nac + "','" + nombre + "','" + enviado + "','" + direccion + "');"
-                    ElseIf ver = 1 And ver2 = 1 Then
-                        Consulta = "INSERT INTO paciente (cedula, fecha_nacimiento, nombre, enviado_por, direccion_particular, direccion_trabajo) values('" + cedula + "','" + nac + "','" + nombre + "','" + enviado + "','" + direccion + "','" + direTra + "');"
-                    End If
-
-
-                    consultar()
-
-                    Consulta = "select id_p from paciente;"
-                    consultar()
-
-                    DataGridView1.DataSource = Tabla
-                    id_p = DataGridView1.Rows(DataGridView1.RowCount - 1).Cells(0).Value
-
-
-                    Consulta = "INSERT INTO antecedentes VALUES ('" + Str(id_p) + "','" + alergicos.GetHashCode.ToString + "','" + diabeticos.GetHashCode.ToString + "','" + cardiovasculares.GetHashCode.ToString + "','" + fiebre_reumatica.GetHashCode.ToString + "','" + coagulacion.GetHashCode.ToString + "','" + odontologicos.GetHashCode.ToString + "','" + farmacos_recibidos.GetHashCode.ToString + "','" + familiares.GetHashCode.ToString + "','" + tratamiento_medico.GetHashCode.ToString + "','" + obs + "');"
-                    consultar()
-
-
-                    MsgBox("Agregado con Exito")
-
-                    Me.Dispose()
-                    Pacientes.Show()
-                    Pacientes.actTabla()
+                    MsgBox("Cédula no válida", MsgBoxStyle.Exclamation)
                 End If
+
             Else
                 MsgBox("La cédula solo debe contener números", MsgBoxStyle.Exclamation)
                 TextBox2.Clear()
@@ -151,4 +156,41 @@
         formu.Dock = DockStyle.Fill
         formu.Show()
     End Sub
+    Private Function verCed(ByVal cedula As String)
+        Try
+            Dim num(7) As Char
+            Dim suma As Integer
+            Dim suma1 As Integer
+            Dim calculo() As Integer = {2, 9, 8, 7, 6, 3, 4}
+
+            num = cedula.ToCharArray()
+
+            For i = 0 To 6
+                Dim num1 As Integer = Val(num(i))
+                suma = (num1 * calculo(i)) + suma
+            Next
+
+            num = suma.ToString.ToCharArray
+
+            ReDim Preserve num(1)
+            suma1 = Convert.ToInt32(num) + 1
+            num = suma1.ToString.ToCharArray
+            ReDim Preserve num(2)
+            num(2) = "0"
+
+            suma1 = Convert.ToInt32(num)
+            suma = suma1 - suma
+            num = cedula.ToCharArray()
+
+            If Str(suma) = Val(num(7)) Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            Return False
+        End Try
+       
+    End Function
+
 End Class
