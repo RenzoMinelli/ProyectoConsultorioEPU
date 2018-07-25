c53788f4af47f639e8dddd39823a5b6a0080cb6d
MateoIzaguirre22 <mateoizaguirre22@outlook.com>
2018-07-25 18:28:42 -0300

    kk

--- proyecto/finanzas/realizar_pago.vb
+++ proyecto/finanzas/realizar_pago.vb
@@ -25,7 +25,7 @@
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        'inputboxcasero.Show()
+        inputboxcasero.Show()
         
        
 
