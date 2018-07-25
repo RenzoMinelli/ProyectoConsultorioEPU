e8c716f32879102667d348f562c29b75671e49cd
MateoIzaguirre22 <mateoizaguirre22@outlook.com>
2018-07-25 17:52:23 -0300

    2

--- proyecto/finanzas/realizar_pago.vb
+++ proyecto/finanzas/realizar_pago.vb
@@ -1,5 +1,5 @@
 ﻿Public Class realizar_pago
-
+    Public id As Integer
     Private Sub realizar_pago_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Consulta = "select id_p , nombre , cedula , saldo from paciente"
         consultar()
@@ -11,23 +11,13 @@
 
     Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
-
+        id = DataGridView1.Rows(DataGridView1.CurrentRow.Index).Cells(0).Value
         Button1.Show()
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         inputboxcasero.Show()
         
-
-        'Mateo el saldo no se tiene que actulizar a lo que se paga :v tenes que restarle al saldo lo que se paga :vvv
-        Consulta = "update paciente set saldo =- '" & monto & "' where id_p = '" & Str(id) & ";"
-        consultar()
-
-        Consulta = "select id_p , nombre , cedula , saldo from paciente"
-        consultar()
-        DataGridView1.DataSource = Tabla
-        DataGridView1.ClearSelection()
-        DataGridView1.Columns(0).Visible = False
 
 
     End Sub
